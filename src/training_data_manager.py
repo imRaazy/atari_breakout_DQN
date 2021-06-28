64c5e0b0d327b92445ec97c72997602e29ebf1b1
Milo <milo.marchetti@libero.it>
2021-06-28 09:58:36 +0200

    Fix indentation

--- src/training_data_manager.py
+++ src/training_data_manager.py
@@ -6,61 +6,61 @@ import pandas as pd
 from collections import deque
 
 class TrainingDataManager:
-    def __init__(self, env, n_aav_states):
-        self.env = env
-        self.aav_states = n_aav_states
-        self.n_episodes = 0
-        self.arpe = 0
-        self.arpe_in_epoch = np.array([])
-        self.aav_in_epoch = np.array([])
-        self.sampled_states = self._initialize_sampled_state()
+  def __init__(self, env, n_aav_states):
+    self.env = env
+    self.aav_states = n_aav_states
+    self.n_episodes = 0
+    self.arpe = 0
+    self.arpe_in_epoch = np.array([])
+    self.aav_in_epoch = np.array([])
+    self.sampled_states = self._initialize_sampled_state()
     
-    def _initialize_sampled_state(self):
-        steps = 5000
-        states = deque(maxlen = steps)
-        state = self.env.reset()
-        for i in range(steps):
-          action = self.env.action_space.sample()
-          state, _, done, _ = self.env.step(action)
-          states.append(state)
-          if done:
-            self.env.reset()
-        indices = np.random.randint(len(states), size=self.aav_states)
-        return [states[index] for index in indices]
+  def _initialize_sampled_state(self):
+    steps = 5000
+    states = deque(maxlen = steps)
+    state = self.env.reset()
+    for i in range(steps):
+      action = self.env.action_space.sample()
+      state, _, done, _ = self.env.step(action)
+      states.append(state)
+      if done:
+        self.env.reset()
+    indices = np.random.randint(len(states), size=self.aav_states)
+    return [states[index] for index in indices]
         
-    def update_aav(self, model):
-        action_values = np.array([])
-        for s in self.sampled_states:
-            state_tensor = tf.expand_dims(tf.convert_to_tensor(s), 0)
-            action_value = np.max(model(state_tensor)[0])
-            action_values = np.append(action_values, action_value)
-        self.aav_in_epoch = np.append(self.aav_in_epoch, np.mean(action_values))
+  def update_aav(self, model):
+    action_values = np.array([])
+    for s in self.sampled_states:
+        state_tensor = tf.expand_dims(tf.convert_to_tensor(s), 0)
+        action_value = np.max(model(state_tensor)[0])
+        action_values = np.append(action_values, action_value)
+    self.aav_in_epoch = np.append(self.aav_in_epoch, np.mean(action_values))
     
-    def update_arpe(self):
-        self.arpe_in_epoch = np.append(self.arpe_in_epoch, self.arpe)
+  def update_arpe(self):
+    self.arpe_in_epoch = np.append(self.arpe_in_epoch, self.arpe)
     
-    def end_episode_update(self, episode_reward):
-        self.n_episodes += 1
-        self.arpe = self.arpe + (episode_reward - self.arpe) / self.n_episodes
+  def end_episode_update(self, episode_reward):
+    self.n_episodes += 1
+    self.arpe = self.arpe + (episode_reward - self.arpe) / self.n_episodes
         
-    def get_aav(self):
-        return self.aav_in_epoch
+  def get_aav(self):
+    return self.aav_in_epoch
         
-    def get_arpe(self):
-        return self.arpe_in_epoch
+  def get_arpe(self):
+    return self.arpe_in_epoch
     
-    def get_episodes(self):
-        return self.n_episodes
+  def get_episodes(self):
+    return self.n_episodes
         
-    def print_results(self):
-        f, (ax1, ax2) = plt.subplots(2, sharex=True)
-        f.tight_layout()
-        ax1.plot(self.arpe_in_epoch)
-        ax1.set_title('Average Reward Per Episode')
-        ax2.plot(self.aav_in_epoch)
-        ax2.set_title('Average Action Value (Q)')
-        ax2.set_xlabel('Epochs')
-        plt.show()
+  def print_results(self):
+    f, (ax1, ax2) = plt.subplots(2, sharex=True)
+    f.tight_layout()
+    ax1.plot(self.arpe_in_epoch)
+    ax1.set_title('Average Reward Per Episode')
+    ax2.plot(self.aav_in_epoch)
+    ax2.set_title('Average Action Value (Q)')
+    ax2.set_xlabel('Epochs')
+    plt.show()
 
 def save_models_and_data(name, folder, model, model_target, arpe, aav):
   path = "./models/" + folder + "/"
